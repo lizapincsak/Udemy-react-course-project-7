990c7adcbc4350575a0c9448da7e1cc10c3c59c3
lizapincsak <liza.pincsak@gmail.com>
2021-08-29 14:54:24 +0200

    children example

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MyParagraph from './MyParagraph';
 
 const DemoOutput = (props) => {
     console.log('Demo Output')
     return  (
-    <p>{props.show ? 'This is new!' : ''}</p>
+    <MyParagraph>{props.show ? 'This is new!' : ''}</MyParagraph>
     );
 };
 
