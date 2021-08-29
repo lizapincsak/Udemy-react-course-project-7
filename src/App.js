40d3c7da7d8155cac04109bcf1e863cae56757c5
lizapincsak <liza.pincsak@gmail.com>
2021-08-29 14:50:26 +0200

    added demo file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from './components/UI/Button/Button';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -9,13 +10,13 @@ function App() {
   console.log('App running')
 
   const toggleParagraphHandler = () => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
